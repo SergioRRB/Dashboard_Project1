66165e9af6acffddf18d751870c09f907acc7958
Sergio Ramos <sergioraulramos276@gmail.com>
2024-02-19 21:10:40 -0500

    Dashboard HTML CSS and JS - PROYECTO 1

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+   {
+      productName: 'Foldable Mini Drone',
+      productNumber: '85631',
+      paymentStatus: 'Due',
+      shipping: 'Pending'
+   },
+   {
+      productName: 'Smartphone Holder',
+      productNumber: '78945',
+      paymentStatus: 'Paid',
+      shipping: 'Shipped'
+   },
+   {
+      productName: 'Wireless Earbuds',
+      productNumber: '65432',
+      paymentStatus: 'Due',
+      shipping: 'Processing'
+   },
+   {
+      productName: 'Coffee Maker',
+      productNumber: '12345',
+      paymentStatus: 'Paid',
+      shipping: 'Delivered'
+   },
+   {
+      productName: 'Fitness Tracker',
+      productNumber: '98765',
+      paymentStatus: 'Due',
+      shipping: 'Pending'
+   },
+   {
+      productName: 'Portable Bluetooth',
+      productNumber: '45678',
+      paymentStatus: 'Paid',
+      shipping: 'Shipped'
+   },
+   {
+      productName: 'Gaming Mouse',
+      productNumber: '78901',
+      paymentStatus: 'Due',
+      shipping: 'Processing'
+   }
+];
